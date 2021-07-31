2c892fdaf37bf713f433095905aa378ae02aab8d
newbee1905 <beenewminh@outlook.com>
2021-08-01 00:32:43 +0700

    Change font && change rules

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "mplus-1m-light:size=13" };
-static const char dmenufont[]       = "mplus-1m-light:size=13";
+static const char *fonts[]          = { "M+ 2m:size=12" };
+static const char dmenufont[]       = "M+ 2m:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -27,10 +27,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   isterminal  noswallow   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           0,          0,          -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,          -1,         -1 },
-	{ "St",       NULL,       NULL,       0,            1,           1,          0,          -1 },
+	/* class         instance    title       tags mask     isfloating   isterminal  noswallow   monitor */
+	{ "Gimp",        NULL,       NULL,       0,            1,           0,          0,          -1 },
+	{ "firefox",     NULL,       NULL,       1 << 8,       0,           0,          -1,         -1 },
+	{ "st-256color", NULL,       NULL,       0,            1,           1,          0,          -1 },
 };
 
 /* layout(s) */
@@ -47,7 +47,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
